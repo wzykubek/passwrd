753b799f8576d08a5da58fcec505c3765578b1dd
samedamci <samedamci@disroot.org>
2021-08-10 00:04:43 +0200

    Initial commit

---
+++ passer/app.py
@@ -0,0 +1,30 @@
+from random import SystemRandom
+
+
+class Passer:
+    def __init__(self, dictionary_file: str) -> None:
+        self.rng = SystemRandom()
+        self.dictionary = self.__get_words_list(dictionary_file)
+        self.symbols = list("""`~1!2@3#4$5%6^7&8*9(0)-_=+\\|]}[{;:'",<.>/?""")
+
+    @staticmethod
+    def __get_words_list(dictionary_path: str) -> list:
+        with open(dictionary_path, "r") as f:
+            dictionary = f.read()
+
+        return dictionary.splitlines()
+
+    @property
+    def random_word(self) -> str:
+        return self.dictionary[self.rng.randint(0, len(self.dictionary))]
+
+    @property
+    def random_symbol(self) -> str:
+        return self.symbols[self.rng.randint(0, len(self.symbols))]
+
+    def gen_password(self, min_length: int) -> str:
+        password = ""
+        while len(password) < min_length:
+            password += self.random_word + self.random_symbol
+
+        return password
